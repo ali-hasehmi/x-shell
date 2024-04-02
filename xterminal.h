8d72d195b4d73728edb50386d343a40336b70b0f
AliReza <AGHashemi.1382@gmail.com>
2024-04-02 18:07:38 +0330

    comments added to xterminal

--- xterminal.h
+++ xterminal.h
@@ -1,11 +1,46 @@
 #ifndef X_TERMINAL_H
 #define X_TERMINAL_H
 
+/*
+    Initalize Default terminal state
+    return 0 on seccuess
+    otherwise return -1;
+*/
 int xterminal_init();
+
+/*
+    reset teh Terminal attributes to state stored by calling xterminal_init
+    return 0 on success
+    otherwise return -1
+*/
 int xterminal_reset();
+
+/*
+    disable line buffering
+    return 0 on success
+    otherwise return -1
+*/
 int xterminal_disable_buffering();
+
+/*
+    disable echoing input keys
+    reutrn 0 on success
+    oterwise return -1
+*/
 int xterminal_disable_echoing();
+
+/*
+    enable echoing input keys
+    reutrn 0 on success
+    oterwise return -1
+*/
 int xterminal_enable_echoing();
+
+/*
+    enable line buffering
+    reutrn 0 on success
+    oterwise return -1
+*/
 int xterminal_enable_buffering();
 
 #endif // X_TERMINAL_H
