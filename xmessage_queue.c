952826f9e0b29905da23f29ebbb2373ba088558e
AliReza <AGHashemi.1382@gmail.com>
2024-04-11 11:10:45 +0330

    xmessage_queue is now thread safe

--- xmessage_queue.c
+++ xmessage_queue.c
@@ -9,8 +9,42 @@
 */
 int xmessage_queue_create(xmessage_queue_t *_queue)
 {
+    if (pthread_mutex_init(&_queue->queue_mutex, NULL))
+    {
+        return -1;
+    }
+    if (pthread_cond_init(&_queue->not_empty, NULL))
+    {
+        return -1;
+    }
+    if (pthread_cond_init(&_queue->not_full, NULL))
+    {
+        return -1;
+    }
     _queue->front = -1;
     _queue->rear = -1;
+    return 0;
+}
+
+/*
+    destroyes the queue
+    return 0 on success
+    return -1 on failure
+*/
+int xmessage_queue_destroy(xmessage_queue_t *_queue)
+{
+    if (pthread_mutex_destroy(&_queue->queue_mutex))
+    {
+        return -1;
+    }
+    if (pthread_cond_destroy(&_queue->not_empty))
+    {
+        return -1;
+    }
+    if (pthread_cond_destroy(&_queue->not_full))
+    {
+        return -1;
+    }
     return 0;
 }
 
@@ -41,12 +75,14 @@ bool xmessage_queue_isFull(xmessage_queue_t *_queue)
 */
 int xmessage_queue_enqueue(xmessage_queue_t *_queue, xmessage_t *_msg)
 {
-    if (xmessage_queue_isFull(_queue))
+    pthread_mutex_lock(&_queue->queue_mutex);
+    while (xmessage_queue_isFull(_queue))
     {
         // wait for queue to have more space
         // but for now just return -1 and print the queue is full
-        printf("Queue is Full!\n");
-        return -1;
+        printf("Queue is Full! waiting to have space\n");
+        pthread_cond_wait(&_queue->not_full, &_queue->queue_mutex);
+        // return -1;
     }
     if (xmessage_queue_isEmpty(_queue))
     {
@@ -57,6 +93,8 @@ int xmessage_queue_enqueue(xmessage_queue_t *_queue, xmessage_t *_msg)
         _queue->rear = (_queue->rear + 1) % MAX_QUEUE_SIZE;
     }
     _queue->buff[_queue->rear] = *_msg;
+    pthread_cond_signal(&_queue->not_empty);
+    pthread_mutex_unlock(&_queue->queue_mutex);
     return 0;
 }
 
@@ -67,12 +105,14 @@ int xmessage_queue_enqueue(xmessage_queue_t *_queue, xmessage_t *_msg)
 */
 int xmessage_queue_dequeue(xmessage_queue_t *_queue, xmessage_t *_msg)
 {
-    if (xmessage_queue_isEmpty(_queue))
+    pthread_mutex_lock(&_queue->queue_mutex);
+    while (xmessage_queue_isEmpty(_queue))
     {
         // wait to have data in queue to return
         // but for now just return -1 and print in terminal
-        printf("Queue is Empty!\n");
-        return -1;
+        printf("Queue is Empty! waiting to enqueue something...\n");
+        pthread_cond_wait(&_queue->not_empty, &_queue->queue_mutex);
+        // return -1;
     }
     if (_msg != NULL)
     {
@@ -88,5 +128,8 @@ int xmessage_queue_dequeue(xmessage_queue_t *_queue, xmessage_t *_msg)
     {
         _queue->front = (_queue->front + 1) % MAX_QUEUE_SIZE;
     }
+    pthread_cond_signal(&_queue->not_full);
+    pthread_mutex_unlock(&_queue->queue_mutex);
+
     return 0;
 }
